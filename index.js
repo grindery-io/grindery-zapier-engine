56233af941e84ce65e5caec90f7d4a7aac380ad7
jmancu01 <juanmancusom@gmail.com>
2022-12-12 16:34:07 -0300

    make it better

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.post("/pushpokeapi", async(req, res)=>{
     shell.exec(`git init `)
     shell.exec(`git pull ${repository}`)
     console.log(path)
+    shell.exec(`npm i`)
     shell.exec(`zapier push`)
     res.sendFile('index.html', {root: __dirname}); 
 })
