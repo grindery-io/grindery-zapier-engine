152e85d594db0e86393e51c4a9fd1cc380a1c612
jmancu01 <juanmancusom@gmail.com>
2022-12-26 10:40:54 -0300

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'; //Import the express dependency
 import shell from 'shelljs'    //for using the shell terminal
+import bp from 'body-parser'
 
 import {updateFile, createFile} from "./src/CRUD.js" //crud files
 import {updateCDS} from "./src/updateCDS.js" //updateCDS
@@ -8,28 +9,9 @@ import {updateCDS} from "./src/updateCDS.js" //updateCDS
 //import {jsondata} from './erc20.json' assert { type: "json" };
 
 const PORT = process.env.PORT || 5000; //define port
-const app = express();              //Instantiate an express app, the main work horse of this server       
-const jsonData = "hello world"
-app.get('/updateCDS', (req, res)=>{
-    const value = updateCDS(jsondata);
-    res.send(value);
-})
-
-// endpoint that runs the function and returns its value
-app.patch('/updateFile', (req, res) => {
-  const value = updateFile(jsondata);
-  res.send(value);
-});
-app.post('/createFile', (req, res) => {
-    const value = createFile(jsondata);
-    res.send(value);
-});
-
-app.post('/githubUpdate', (req, res) => {
-    const value = ""
-    res.send(value);
-});
-
+const app = express();              //Instantiate an express app, the main work horse of this server      
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
 app.post("/push", async(req, res)=>{
     const repository = req.query.repository
@@ -60,17 +42,40 @@ app.post("/pushpokeapi", async(req, res)=>{
     res.sendFile('index.html', {root: __dirname}); 
 })
 
-app.post('/githubUpdate', (req, res) => {
-    console.log(req.body)
-    const value = JSON.parse(req.body)
+app.post('/githubUpdate', async(req, res) => {
+    const value = req.body
     const added = value.commits.added
+    console.log(added)
     const removed = value.commits.removed
     console.log(removed)
     const modified = value.commits.modified
     console.log(modified)
-    res.send(added);
+    const obj = {
+        added: added,
+        removed: removed,
+        modified: modified
+    }
+    console.log(obj);
 });
 
 app.listen(PORT, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${PORT}`); 
 });
+
+
+
+// const jsonData = "hello world"
+// app.get('/updateCDS', (req, res)=>{
+//     const value = updateCDS(jsondata);
+//     res.send(value);
+// })
+
+// // endpoint that runs the function and returns its value
+// app.patch('/updateFile', (req, res) => {
+//   const value = updateFile(jsondata);
+//   res.send(value);
+// });
+// app.post('/createFile', (req, res) => {
+//     const value = createFile(jsondata);
+//     res.send(value);
+// });
