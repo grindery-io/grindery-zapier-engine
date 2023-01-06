6dcb9995ab562628f4fdcdb628e9cb35583a61dc
jmancu01 <juanmancusom@gmail.com>
2023-01-06 17:38:07 -0300

    check if trigger

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ app.post("/githubUpdate", async (req, res) => {
     
     // push to zapier
     await pushDynamic();
-    await sendNotification()
+    //await sendNotification()
     
     res.status(200).json({"res": "hello"})
   }else{
