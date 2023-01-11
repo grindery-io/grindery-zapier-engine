edc7178bf08080a85748f12add6d354e7397c62a
jmancu01 <juanmancusom@gmail.com>
2023-01-11 15:42:25 -0300

    push actions

--- actionHiddenTemplate.js
+++ actionHiddenTemplate.js
@@ -36,7 +36,7 @@ const perform = async (z, bundle) => {
 module.exports = {
   // see here for a full list of available properties:
   // https://github.com/zapier/zapier-platform/blob/master/packages/schema/docs/build/schema.md#triggerschema
-  key: "replaceAction",
+  key: "replaceAction_action_hidden",
   noun: "replaceAction Actions",
 
   display: {
