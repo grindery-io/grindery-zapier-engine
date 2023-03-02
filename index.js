29dfa1834ebd2fad7f96bd9db1715ad84f349fe9
jmancu01 <juanmancusom@gmail.com>
2023-03-02 09:05:28 -0600

    to production

--- index.js
+++ index.js
@@ -508,8 +508,8 @@ async function runPayload(value){
 }
 
 app.post("/githubUpdate", async (req, res) => {
-  //const value = JSON.parse(req.body.payload); //PRODUCTION
-  const value = req.body; //TESTING POSTMAN
+  const value = JSON.parse(req.body.payload); //PRODUCTION
+  //const value = req.body; //TESTING POSTMAN
 
   runPayload(value);
   res.status(200).json({ res: "Payload Received successfully. Processing..." });
